6c8575270fa47be68bf9a1e5e0ad25df4be2d2d9
Rubanov <bykr3m@gmail.com>
2026-05-03 17:16:55 +0300

    Added system for enemy abilities

---
+++ Scenes/Components/enemy_ability_picker.gd
@@ -0,0 +1,46 @@
+class_name EnemyAbilityPicker
+extends Node
+
+var total_weight : int = 0
+
+func _ready() -> void:
+	set_weights()
+
+func set_weights() -> void:
+	var weight_value : int = 0
+	
+	for ability : EnemyAbility in get_children():
+		if ability != null and ability.type == EnemyAbility.Type.CHANCEBASED:
+			weight_value += ability.weight
+			ability.accumulated_weight = weight_value
+	
+	total_weight = weight_value
+
+func pick_ability() -> EnemyAbility:
+	var ability = pick_conditional()
+	if ability != null:
+		return ability
+	
+	ability = pick_chancebased()
+	if ability != null:
+		return ability
+	
+	return null
+
+func pick_conditional() -> EnemyAbility:
+	for ability : EnemyAbility in get_children():
+		if ability != null and ability.type == EnemyAbility.Type.CONDITIONAL:
+			if ability.is_applicapable():
+				return ability
+	
+	return null
+	
+func pick_chancebased() -> EnemyAbility:
+	var roll = randi_range(0, total_weight)
+	
+	for ability : EnemyAbility in get_children():
+		if ability != null and ability.type == EnemyAbility.Type.CHANCEBASED:
+			if roll < ability.accumulated_weight:
+				return ability
+	
+	return null
---
+++ Scripts/EnemyAbilities/EnemyAbility.gd
@@ -0,0 +1,17 @@
+class_name EnemyAbility
+extends Node
+
+enum Type {CONDITIONAL, CHANCEBASED}
+
+@export var type : Type
+@export_range(1,100,1) var weight : int
+var accumulated_weight : int = 0
+
+var enemy : Enemy
+var target : Node2D
+
+func is_applicapable() -> bool:
+	return false
+
+func apply() -> void:
+	pass
